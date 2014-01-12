858707836b18bd02f26e0389e421645050c69dfd
Nafis Zaman <nafis.zaman-1@ou.edu>
2014-01-12 07:24:01 -0600

    Bump to version 1.2.0

--- include/cs2.hpp
+++ include/cs2.hpp
@@ -2,8 +2,8 @@
 #define _CS2_HPP_
 
 #define CS2_VERSION_MAJOR 1
-#define CS2_VERSION_MINOR 1
-#define CS2_VERSION_BUILD 6
+#define CS2_VERSION_MINOR 2
+#define CS2_VERSION_BUILD 0
 
 #define SERVER_ID_FILE (KAR_SPECIAL_PREFIX "id")
 
